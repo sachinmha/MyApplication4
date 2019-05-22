05d4b9de1c6ddeef13db741c7c8ebcde2ea1f713
manish.kumar <manish.kr@velocis.co.in>
2019-05-22 15:51:21 +0530

    Create MyModel

---
+++ app/src/main/java/image/com/myapplication/MyModel.java
@@ -0,0 +1,23 @@
+package image.com.myapplication;
+
+public class MyModel {
+
+    private int roll;
+    private int name;
+
+    public int getRoll() {
+        return roll;
+    }
+
+    public void setRoll(int roll) {
+        this.roll = roll;
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+}
