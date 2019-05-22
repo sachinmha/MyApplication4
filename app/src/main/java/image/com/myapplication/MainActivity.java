84e9aee8099671300617fd31032074a46356cd4b
manish.kumar <manish.kr@velocis.co.in>
2019-05-22 15:41:54 +0530

    Manish comments

--- app/src/main/java/image/com/myapplication/MainActivity.java
+++ app/src/main/java/image/com/myapplication/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("HEllo");
+
+        System.out.println("Welocme");
     }
 }
